e6939a27820e115e7ac90736cc0ef2f41deefd73
Verissimo Santos <joseverissimo@grupognc.com.br>
2021-06-09 08:33:49 -0300

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 //Modules
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
